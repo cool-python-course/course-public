b83185a12f43e92522c40ef8d5bb7ec47044dcb6
Zsolt Toth <tothzs87@gmail.com>
2021-10-13 20:58:41 +0200

    feat(web): add calculator end points and service

--- web/calculator/main.py
+++ web/calculator/main.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from calculator_router import calculator_api
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """
+    curl 'http://localhost:5001/' --verbose
+    :return:
+    """
     return 'Hello World'
 
 if __name__ == '__main__':
-
     print(f'Hello World')
-    app.run(host='0.0.0.0',port=3000, debug=True)
+    app.register_blueprint(calculator_api)
+    app.run(host='0.0.0.0',port=5001, debug=True)
