7c27efaeabd01307a42f8a58296e6c3fe405e6ee
Zsolt Toth <tothzs87@gmail.com>
2021-10-20 19:05:11 +0200

    feat(parking): add parking plotting

--- data-science/exploratory-analysis/main.py
+++ data-science/exploratory-analysis/main.py
@@ -6,4 +6,12 @@ import parking
 
 
 if __name__ == '__main__':
-    parking_ds = parking.read_dataset()
+    # dataset = parking.read_dataset()
+
+    PARKING_CODE = 'Shopping'
+    SELECTED_DATE = '2016-12-19'
+    parking.plot_occupancy_of_parking_on_day(PARKING_CODE, SELECTED_DATE)
+
+
+
+
--- data-science/exploratory-analysis/parking.py
+++ data-science/exploratory-analysis/parking.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def round_time_to_n_mins(time : datetime.datetime, n: int) -> datetime:
     return (time - datetime.timedelta(
@@ -20,8 +22,20 @@ def read_dataset() -> pd.DataFrame:
         },
         parse_dates=['LastUpdated']
     )
-    parking['date'] = parking['LastUpdated'].dt.date
-    parking['time'] = parking['LastUpdated'].dt.time
+    parking['date'] = pd.to_datetime(parking['LastUpdated'].dt.date)
+    parking['time'] = pd.to_timedelta(parking['LastUpdated'].dt.time, errors='coerce')
     parking['timeFuzzy'] = parking.apply(lambda row: round_time_to_n_mins(row['LastUpdated'], 5), axis=1)
+    parking['minuteOfDay'] = parking.apply(lambda row: 60 * (row['timeFuzzy'].hour) + row['timeFuzzy'].minute, axis =1)
     parking['freeSlots'] = parking['Capacity'] - parking['Occupancy']
     return parking
+
+def plot_occupancy_of_parking_on_day(parking_code_number : str, day : str) -> None:
+    dataset = read_dataset()
+    usage = dataset.loc[(dataset['date'] == day) & (dataset['SystemCodeNumber'] == parking_code_number)]
+    sns.set()
+    p = sns.lineplot(data=usage, x='minuteOfDay', y='Occupancy')
+    p = sns.lineplot(data=usage, x='minuteOfDay', y='freeSlots')
+    p.set_xlabel('Minute of Day')
+    p.set_ylabel('# Cars')
+    plt.title(f'{parking_code_number} -- {day}')
+    plt.show()
